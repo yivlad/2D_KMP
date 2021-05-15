09bc9ac4a2de1cdf7b1bb1fb46093fc2250819cc
k1kli <karolklimaszewski0@gmail.com>
2021-05-16 01:07:33 +0200

    Add overlap parameter

--- input_generator.py
+++ input_generator.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 from os import path, mkdir
 from random import randint
 from sys import stderr
@@ -33,6 +34,8 @@ def parse_args():
                         help="Maximum value of an element (default=10)", default=10)
     parser.add_argument("--appearances", type=int, dest="appearances",
                         help="Attempted number of appearances of pattern in search matrix (default=5)", default=5)
+    parser.add_argument("--overlap", action="store_true", dest="overlap",
+                        help="If present overlapping appearances of pattern in search matrix will be attempted")
     args = parser.parse_args()
     if args.search_matrix_width < args.pattern_matrix_width or args.search_matrix_width < args.pattern_matrix_width:
         print(f"Invalid args, search matrix too small", file=stderr)
@@ -58,11 +61,39 @@ def mat_to_str(mat):
 def create_matrices(search_h, search_w, pattern_h, pattern_w, max_el, appearances):
     search = [[randint(0, max_el) for _ in range(search_w)] for _ in range(search_h)]
     pattern = [[randint(0, max_el) for _ in range(pattern_w)] for _ in range(pattern_h)]
-    for _ in range(0, appearances):
-        y_0 = randint(0, search_h - pattern_h)
-        x_0 = randint(0, search_w - pattern_w)
-        for y in range(y_0, y_0 + pattern_h):
-            search[y][x_0:(x_0 + pattern_w)] = pattern[y - y_0]
+    overlap_chance = 1 / appearances
+    i = 0
+    while i < appearances:
+        overlap = random.random() <= overlap_chance
+        if overlap:
+            i = i + 1
+            max_overlap_w = pattern_w//2
+            max_overlap_h = pattern_h//2
+            min_overlap_w = max(1, pattern_w*2-search_w)
+            min_overlap_h = max(1, pattern_h*2-search_h)
+            if min_overlap_w > max_overlap_w or min_overlap_h > max_overlap_h:
+                print("Invalid dimensions for overlap", file=stderr)
+                exit(-1)
+            overlap_w = randint(min_overlap_w, max_overlap_w)
+            overlap_h = randint(min_overlap_h, max_overlap_h)
+            overlap_x0 = pattern_w - overlap_w
+            overlap_y0 = pattern_h - overlap_h
+            for y in range(overlap_y0, pattern_h):
+                pattern[y][overlap_x0:pattern_w] = pattern[y-overlap_y0][0:overlap_w]
+            y_0 = randint(0, search_h - pattern_h - overlap_x0)
+            x_0 = randint(0, search_w - pattern_w - overlap_y0)
+            y_1 = y_0+overlap_y0
+            x_1 = x_0+overlap_x0
+            for y in range(y_0, y_0 + pattern_h):
+                search[y][x_0:(x_0 + pattern_w)] = pattern[y - y_0]
+            for y in range(y_1, y_1 + pattern_h):
+                search[y][x_1:(x_1 + pattern_w)] = pattern[y - y_1]
+        else:
+            y_0 = randint(0, search_h - pattern_h)
+            x_0 = randint(0, search_w - pattern_w)
+            for y in range(y_0, y_0 + pattern_h):
+                search[y][x_0:(x_0 + pattern_w)] = pattern[y - y_0]
+        i = i + 1
     return search, pattern
 
 
