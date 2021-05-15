fef54d038b8fa0827bc28da7905c9f0573d3c408
k1kli <karolklimaszewski0@gmail.com>
2021-05-16 00:13:04 +0200

    Add maximum element value parameter

--- input_generator.py
+++ input_generator.py
@@ -11,7 +11,8 @@ def main():
         file_path = path.join(args.results_directory, f"input{i}.txt")
         with open(file_path, "w") as file:
             search, pattern = create_matrices(args.search_matrix_height, args.search_matrix_width,
-                                              args.pattern_matrix_height, args.pattern_matrix_height)
+                                              args.pattern_matrix_height, args.pattern_matrix_height,
+                                              args.max_element_value)
             file_contents = to_file(search, pattern)
             file.write(file_contents)
 
@@ -28,6 +29,8 @@ def parse_args():
                         help="Height of pattern matrix in generated files")
     parser.add_argument("pattern_matrix_width", metavar="B", type=int,
                         help="Width of pattern matrix in generated files")
+    parser.add_argument("max_element_value", metavar="MAX_EL", type=int,
+                        help="Maximum value of an element (default=10)", default=10)
     args = parser.parse_args()
     if args.search_matrix_width < args.pattern_matrix_width or args.search_matrix_width < args.pattern_matrix_width:
         print(f"Invalid args, search matrix too small", file=stderr)
@@ -50,9 +53,9 @@ def mat_to_str(mat):
     return "\n".join([" ".join([str(el) for el in row]) for row in mat])
 
 
-def create_matrices(search_h, search_w, pattern_h, pattern_w):
-    search = [[randint(0, 10) for _ in range(search_w)] for _ in range(search_h)]
-    pattern = [[randint(0, 10) for _ in range(pattern_w)] for _ in range(pattern_h)]
+def create_matrices(search_h, search_w, pattern_h, pattern_w, max_el):
+    search = [[randint(0, max_el) for _ in range(search_w)] for _ in range(search_h)]
+    pattern = [[randint(0, max_el) for _ in range(pattern_w)] for _ in range(pattern_h)]
     for _ in range(0, 5):
         y_0 = randint(0, search_h - pattern_h)
         x_0 = randint(0, search_w - pattern_w)
